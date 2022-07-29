f07e21249db9507f18bcf89c7a1bf833eccf571d
huaixin <libinsong1204@gmail.com>
2022-07-29 11:47:58 +0800

    支持join

--- src/main/kotlin/com/github/melin/superior/sql/formatter/spark/FormatterVisitor.kt
+++ src/main/kotlin/com/github/melin/superior/sql/formatter/spark/FormatterVisitor.kt
@@ -1,13 +1,14 @@
 package com.github.melin.superior.sql.formatter.spark
 
+import com.github.melin.superior.sql.formatter.spark.SparkSqlParser.NamedExpressionContext
 import com.github.melin.superior.sql.formatter.spark.SparkSqlParser.SetQuantifierContext
-import com.github.melin.superior.sql.formatter.spark.SparkSqlParser.TableNameContext
 import com.google.common.base.Strings
 import com.google.common.collect.Iterables
+import org.antlr.v4.runtime.tree.TerminalNodeImpl
 
 class FormatterVisitor(val builder: StringBuilder) : SparkSqlParserBaseVisitor<Void>() {
     companion object {
-        private const val INDENT = "   "
+        private const val INDENT = "  "
     }
 
     private var indent: Int = 0
@@ -19,38 +20,108 @@ class FormatterVisitor(val builder: StringBuilder) : SparkSqlParserBaseVisitor<V
 
     override fun visitSetQuantifier(ctx: SetQuantifierContext): Void? {
         if (ctx.DISTINCT() !== null) {
-            builder.append(" DISTINCT")
+            append(indent, " DISTINCT")
         }
         return null
     }
 
     override fun visitFromClause(ctx: SparkSqlParser.FromClauseContext): Void? {
         append(indent, "FROM")
-        builder.append('\n')
-        append(indent, "  ")
+        append(indent, "\n", INDENT)
         return super.visitFromClause(ctx)
     }
 
-    override fun visitRelation(ctx: SparkSqlParser.RelationContext): Void? {
-        var node = ctx.getChild(0)
-        if (node is TableNameContext) {
-            builder.append(node.multipartIdentifier().text)
-            return null
-        } else {
-            return super.visitRelation(ctx)
+    override fun visitJoinRelation(ctx: SparkSqlParser.JoinRelationContext): Void? {
+        append(indent, "\n", INDENT)
+        ctx.children.forEach { child ->
+            if (child is TerminalNodeImpl) {
+                builder.append(child.text.uppercase()).append(" ")
+            } else {
+                visit(child)
+            }
         }
+        return null
     }
 
-    override fun visitNamedExpression(ctx: SparkSqlParser.NamedExpressionContext): Void? {
-        if (ctx.childCount > 1) {
+    override fun visitJoinType(ctx: SparkSqlParser.JoinTypeContext): Void? {
+        ctx.children.forEach { child ->
+            builder.append(child.text.uppercase()).append(" ")
+        }
+        return null
+    }
 
+    override fun visitJoinCriteria(ctx: SparkSqlParser.JoinCriteriaContext): Void? {
+        builder.append(" ")
+        ctx.children.forEach { child ->
+            if (child is TerminalNodeImpl) {
+                builder.append(child.text.uppercase()).append(" ")
+            } else {
+                visit(child)
+            }
+        }
+        return null
+    }
+
+    override fun visitComparisonOperator(ctx: SparkSqlParser.ComparisonOperatorContext): Void? {
+        builder.append(" ").append(ctx.text).append(" ")
+        return null
+    }
+
+    override fun visitPredicate(ctx: SparkSqlParser.PredicateContext?): Void {
+        return super.visitPredicate(ctx)
+    }
+
+    override fun visitRelation(ctx: SparkSqlParser.RelationContext): Void? {
+        return super.visitRelation(ctx)
+    }
+
+    override fun visitTableAlias(ctx: SparkSqlParser.TableAliasContext): Void? {
+        ctx.children.forEach { child ->
+            if (child is TerminalNodeImpl) {
+                builder.append(" ").append(child.text.uppercase())
+            } else {
+                builder.append(" ").append(child.text)
+            }
+        }
+        return null;
+    }
+
+    override fun visitNamedExpressionSeq(ctx: SparkSqlParser.NamedExpressionSeqContext): Void? {
+        if (ctx.childCount > 1) {
+            ctx.children.forEach { child ->
+                if (child is NamedExpressionContext) {
+                    append(indent, "\n", INDENT)
+                    visit(child)
+                } else if (child is TerminalNodeImpl) {
+                    builder.append(child.text)
+                } else {
+                    builder.append("")
+                }
+            }
         } else {
             builder.append(' ')
             visit(Iterables.getOnlyElement(ctx.children))
         }
         builder.append('\n')
 
-        return null
+        return null;
+    }
+
+    override fun visitDereference(ctx: SparkSqlParser.DereferenceContext): Void? {
+        ctx.children.forEach { child ->
+            if (child is TerminalNodeImpl) {
+                builder.append(child.text)
+            } else {
+                visit(child)
+            }
+        }
+
+        return null;
+    }
+
+    override fun visitIdentifier(ctx: SparkSqlParser.IdentifierContext): Void? {
+        builder.append(ctx.text)
+        return null;
     }
 
     override fun visitColumnReference(ctx: SparkSqlParser.ColumnReferenceContext): Void? {
@@ -58,9 +129,19 @@ class FormatterVisitor(val builder: StringBuilder) : SparkSqlParserBaseVisitor<V
         return null;
     }
 
+    override fun visitStar(ctx: SparkSqlParser.StarContext): Void? {
+        builder.append(ctx.text)
+        return null;
+    }
+
     private fun append(indent: Int, value: String): java.lang.StringBuilder? {
         return builder.append(indentString(indent))
             .append(value)
+    }
+
+    private fun append(indent: Int, value1: String, value2: String): java.lang.StringBuilder? {
+        return builder.append(indentString(indent))
+            .append(value1).append(value2)
     }
 
     private fun indentString(indent: Int): String? {
--- src/test/kotlin/com/github/melin/superior/sql/formatter/spark/SparkSqlFormatterTest.kt
+++ src/test/kotlin/com/github/melin/superior/sql/formatter/spark/SparkSqlFormatterTest.kt
@@ -16,4 +16,36 @@ class SparkSqlFormatterTest {
         """.trimMargin()
         Assert.assertEquals(expected, formatSql)
     }
+
+    @Test
+    fun simpleSelectSqlTest1() {
+        val sql = "select distinct name, age from users as t"
+        val formatSql = SparkSqlFormatter.formatSql(sql)
+        val expected = """
+            |SELECT DISTINCT
+            |  name,
+            |  age
+            |FROM
+            |  users AS t
+        """.trimMargin()
+        Assert.assertEquals(expected, formatSql)
+    }
+
+    @Test
+    fun joinSelectSqlTest() {
+        val sql = """
+            SELECT * FROM demo1 as t1
+            LEFT JOIN demo2 t2 on t1.col1 = t2.col2
+            left join demo3 t3 on t1.sd = t3.col
+        """.trimIndent()
+        val formatSql = SparkSqlFormatter.formatSql(sql)
+        val expected = """
+            |SELECT *
+            |FROM
+            |  demo1 AS t1
+            |  LEFT JOIN demo2 t2 ON t1.col1 = t2.col2
+            |  LEFT JOIN demo3 t3 ON t1.sd = t3.col
+        """.trimMargin()
+        Assert.assertEquals(expected, formatSql)
+    }
 }
