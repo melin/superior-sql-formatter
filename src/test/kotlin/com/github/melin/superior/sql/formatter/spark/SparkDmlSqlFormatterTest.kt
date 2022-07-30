45a6f5d2cf08051901a9f449e6c14c4c63dce7c8
huaixin <libinsong1204@gmail.com>
2022-07-30 21:48:14 +0800

    重构类名

--- src/test/kotlin/com/github/melin/superior/sql/formatter/spark/SparkDmlSqlFormatterTest.kt
+++ src/test/kotlin/com/github/melin/superior/sql/formatter/spark/SparkDmlSqlFormatterTest.kt
@@ -8,7 +8,7 @@ class SparkDmlSqlFormatterTest {
     @Test
     fun simpleSelectSqlTest() {
         val sql = "SELECT name, age FROM person ORDER BY age DESC, name asc NULLS FIRST;"
-        val formatSql = SparkSqlFormatter.formatSql(sql)
+        val formatSql = SparkSelectSqlFormatter.formatSql(sql)
         val expected = """
             |SELECT
             |  name,
@@ -25,7 +25,7 @@ class SparkDmlSqlFormatterTest {
     @Test
     fun simpleSelectSqlTest1() {
         val sql = "select distinct name, age from users as t;"
-        val formatSql = SparkSqlFormatter.formatSql(sql)
+        val formatSql = SparkSelectSqlFormatter.formatSql(sql)
         val expected = """
             |SELECT DISTINCT
             |  name,
@@ -39,7 +39,7 @@ class SparkDmlSqlFormatterTest {
     @Test
     fun simpleSelectSqlTest2() {
         val sql = "select date '2022-12-12' as test, name from demo where not name = 'ss' and id>100"
-        val formatSql = SparkSqlFormatter.formatSql(sql)
+        val formatSql = SparkSelectSqlFormatter.formatSql(sql)
         val expected = """
             |SELECT
             |  date '2022-12-12' AS test,
@@ -57,7 +57,7 @@ class SparkDmlSqlFormatterTest {
     fun simpleSelectGroupSqlTest1() {
         val sql = "SELECT id, sum(quantity) filter (WHERE car_model IN ('Honda Civic', 'Honda CRV')) AS `sum(quantity)` " +
                 "FROM dealer GROUP BY id ORDER BY id;"
-        val formatSql = SparkSqlFormatter.formatSql(sql)
+        val formatSql = SparkSelectSqlFormatter.formatSql(sql)
         val expected = """
             |SELECT
             |  id,
@@ -79,7 +79,7 @@ class SparkDmlSqlFormatterTest {
     fun simpleSelectGroupSqlTest2() {
         val sql = "SELECT city, car_model, sum(quantity) AS sum FROM dealer " +
                 "GROUP BY GROUPING SETS ((city, car_model), (city), (car_model), ()) ORDER BY city"
-        val formatSql = SparkSqlFormatter.formatSql(sql)
+        val formatSql = SparkSelectSqlFormatter.formatSql(sql)
         val expected = """
             |SELECT
             |  city,
@@ -104,7 +104,7 @@ class SparkDmlSqlFormatterTest {
     fun simpleSelectGroupSqlTest3() {
         val sql = "SELECT city, car_model, sum(quantity) AS sum FROM dealer " +
                 "GROUP BY city, car_model WITH ROLLUP ORDER BY city, car_model;"
-        val formatSql = SparkSqlFormatter.formatSql(sql)
+        val formatSql = SparkSelectSqlFormatter.formatSql(sql)
         val expected = """
             |SELECT
             |  city,
@@ -125,7 +125,7 @@ class SparkDmlSqlFormatterTest {
     @Test
     fun subQuerySqlTest() {
         val sql = "select d.name, d.test from (select * from demo) t"
-        val formatSql = SparkSqlFormatter.formatSql(sql)
+        val formatSql = SparkSelectSqlFormatter.formatSql(sql)
         val expected = """
             |SELECT
             |  d.name,
@@ -147,7 +147,7 @@ class SparkDmlSqlFormatterTest {
             LEFT JOIN demo2 t2 on t1.col1 = t2.col2
             left join demo3 t3 on t1.sd = t3.col
         """.trimIndent()
-        val formatSql = SparkSqlFormatter.formatSql(sql)
+        val formatSql = SparkSelectSqlFormatter.formatSql(sql)
         val expected = """
             |SELECT *
             |FROM
@@ -164,7 +164,7 @@ class SparkDmlSqlFormatterTest {
             SELECT * FROM sales.orders WHERE customer_id IN (SELECT customer_id FROM sales.customers WHERE city = 'San Jose')
             ORDER BY customer_id, order_date
         """.trimIndent()
-        val formatSql = SparkSqlFormatter.formatSql(sql)
+        val formatSql = SparkSelectSqlFormatter.formatSql(sql)
         val expected = """
             |SELECT *
             |FROM
@@ -190,7 +190,7 @@ class SparkDmlSqlFormatterTest {
             SELECT * FROM sales.orders o WHERE EXISTS (SELECT customer_id FROM sales.customers c WHERE o.customer_id = c.customer_id AND city = 'San Jose')
             ORDER BY o.customer_id, order_date
         """.trimIndent()
-        val formatSql = SparkSqlFormatter.formatSql(sql)
+        val formatSql = SparkSelectSqlFormatter.formatSql(sql)
         val expected = """
             |SELECT *
             |FROM
